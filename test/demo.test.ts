b4c969943c0b7db9f7d740f294af155bf0d511c5
YEP-Autumn <2017248646s@gmail.com>
2025-09-05 02:36:27 -0400

    add demo test

---
+++ test/demo.test.ts
@@ -0,0 +1,25 @@
+import { TestHelper } from "../utils/TestHelper";
+
+let testHelper: TestHelper;
+
+beforeAll(async () => {
+  testHelper = await new TestHelper().init();
+});
+
+afterAll(async () => {
+  await testHelper.destroy();
+});
+
+test("测试demo功能", async () => {
+  testHelper.ExecConfigDutA(["configure terminal", "end"]);
+
+  testHelper.sleep(5000);
+
+  testHelper.ExecRetMatchDutA("ping 127.0.0.1", / 0% packet loss/g);
+
+  testHelper.CleanConfigDutA(["configure terminal", "end"]);
+
+  await testHelper.startTest();
+
+  expect(testHelper.result()).toBeTruthy();
+}, 300000);

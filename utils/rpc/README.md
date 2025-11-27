59c150a49eb97d246ab691cb66ed8701dde6d662
YEP-Autumn <2017248646s@gmail.com>
2025-11-27 07:02:48 -0500

    update some file

--- utils/rpc/README.md
+++ utils/rpc/README.md
@@ -2,4 +2,6 @@
 thrift --gen js thrift/tutorial.thrift
 thrift --gen js:node thrift/tutorial.thrift
 thrift --gen js:ts thrift/tutorial.thrift
+
+thrift --gen js:ts thrift/packet.thrift
 ```
--- utils/stream/packet.ts
+++ utils/stream/packet.ts
@@ -1,8 +1,31 @@
-class EthernetII {}
-class MPLS {}
-class VLAN {}
-class IPv4 {}
-class IPv6 {}
-class TCP {}
-class UDP {}
-class ICMP {}
+// 假设 Thrift 库和生成的代码已经导入
+const thrift = require("thrift");
+require("../rpc/gen-js/packet_types");
+
+const TCompactProtocol = thrift.TCompactProtocol;
+const TBufferedTransport = thrift.TBufferedTransport; // 用于示例
+
+// 从全局作用域获取 Thrift 生成的类
+const { Packet, EthernetII, VLAN, IPv4, FieldMeta, Field } = globalThis as any;
+
+let packet = new Packet();
+
+let ethernet = new EthernetII();
+ethernet.src = "0001.0203.1002";
+ethernet.dst = "0001.0203.1003";
+ethernet.type = 0x8100;
+console.log(ethernet);
+
+let vlan = new VLAN();
+vlan.vlan = 100;
+vlan.type = 0x0800;
+console.log(vlan);
+
+let ipv4 = new IPv4();
+ipv4.src_ip = 100000;
+ipv4.dst_ip = 200000;
+console.log(ipv4);
+
+let fieldMeta = new FieldMeta();
+let field = new Field();
+
